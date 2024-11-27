0a4d9cb19d227f21b19435a9534cc036f6a640a5
Sertan Ortaoğlan <sertanortaoglan@hotmail.com>
2024-11-28 00:57:53 +0300

    second lecture commit

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,42 +1,33 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+//import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity  //Bu anotasyon 'Brand' class'ının bir veritabana nesnesi olduğunu belirtir.
+@Table(name = "brands") //Bu 'Brand' class'ı veritabanında 'brands' isimli tabloya karşılık gelir demektir.
+//@Data(Getter, Setter ve parametresiz constructor oluşturur.)
+@Getter
+@Setter
+@AllArgsConstructor //Parametreli constructor oluşturur. 
+@NoArgsConstructor	//Parametresiz constructor oluşturur. 
 public class Brand {
+	@Id  //Veritabanında 'primary key'(PK) alanına denk geliyorsun demektir.
+	@GeneratedValue(strategy = GenerationType.IDENTITY)   //Veritabanında id'yi otomatik olarak birer birer arttır demektir.
+	@Column(name = "id")
 	private int id;
+	
+	@Column(name = "name")
 	private String name;
-	
-	public Brand() {
-	}
-
-	public Brand(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
+
+
+
